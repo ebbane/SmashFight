730d310fe74a44a89e13f83241fa6dea05157130
Ebbane Diet <ebbanediet@gmailcom>
2021-05-23 22:20:40 +0200

    Add bdd functions

--- tkinter/Projet SmashFight/database.py
+++ tkinter/Projet SmashFight/database.py
@@ -1,15 +1,94 @@
 import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="projetLogiciel",
-  password="hfX5MfGPNO6Q3mD9",
-   database="SmashFight"
-)
-mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), score int)") 
-mycursor.execute("SHOW TABLES")
 
-for x in mycursor:
-  print(x) 
+def disp():
+    try:
+        mySQLconnection = mysql.connector.connect(
+                                host="localhost",
+                                user="projetLogiciel",
+                                password="hfX5MfGPNO6Q3mD9",
+                                database="SmashFight"
+        )
+
+        sql_select_Query = "SELECT * FROM users ORDER BY score DESC"
+        cursor = mySQLconnection.cursor()
+        cursor.execute(sql_select_Query)
+        records = cursor.fetchall()
+
+        print("Total number of rows in student is - ", cursor.rowcount)
+        print ("Printing each row's column values i.e.  student record")
+
+        for row in records:
+            print(row[0],"\t",row[1],"\t""\n")
+
+        cursor.close()
+    
+    except Error as e :
+        print ("Error while connecting to MySQL", e)
+    finally:
+        #closing database connection.
+        if(mySQLconnection.is_connected()):
+            mySQLconnection.close()
+            print("MySQL connection is closed")
+
+def insert(name,score):
+    global x
+    try:
+       connection = mysql.connector.connect(
+                                host="localhost",
+                                user="projetLogiciel",
+                                password="hfX5MfGPNO6Q3mD9",
+                                database="SmashFight"
+        )
+
+    
+       sql_insert_query = ("INSERT INTO users(name, score) VALUES (%s,%s)", (name, score))
+
+       cursor = connection.cursor()
+       result  = cursor.execute(*sql_insert_query)
+       connection.commit()
+       print ("Succes")
+
+    except mysql.connector.Error as error :
+        connection.rollback() #rollback if any exception occured
+        print("Failed inserting record into users table {}".format(error))
+
+    finally:
+        #closing database connection.
+        if(connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+def update(name,score):
+    try:
+       connection = mysql.connector.connect(
+                                host="localhost",
+                                user="projetLogiciel",
+                                password="hfX5MfGPNO6Q3mD9",
+                                database="SmashFight"
+        )
+
+       cursor = connection.cursor()
+
+       #Update single record now
+       sql_update_query = "Update users set score = '' where name = '' "
+       cursor.execute(sql_update_query)
+       connection.commit()
+       print ("Record Updated successfully ")
+
+       
+
+    except mysql.connector.Error as error :
+        print("Failed to update record to database: {}".format(error))
+        connection.rollback()
+
+    finally:
+        #closing database connection.
+        if(connection.is_connected()):
+            connection.close()
+            print("connection is closed")
